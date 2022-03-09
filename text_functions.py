ba6008ae1e92435822d53382a0f8e7b9068c9857
maddoxamei <maddoxamei@gmail.com>
2022-03-09 11:05:33 -0500

    Save/Load Serialized Corpus
    Add option to omit number_of_texts, Handle case where #texts < #ids.
    Rename functions so that capitalization is consistent with other work and more descriptive
    Added pickle serialization

--- text_functions.py
+++ text_functions.py
@@ -1,20 +1,35 @@
 from text_objects import Book, FanFiction
+import pickle
 
-def _get_Books(number_of_texts, book_ids = [], **kwargs):
-	books = [Book( text_id = idx ) for idx in book_ids]
-	books.extend(Book() for x in range(number_of_texts - len(books)))
+def _scrape_books(number_of_texts = 0, book_ids = [], min_book_popularity = 0, **kwargs):
+	books = [Book( text_id = idx, min_popular = min_book_popularity ) for idx in book_ids]
+	more = max( number_of_texts - len(books), 0 )
+	books.extend(Book() for x in range(more))
 	return books
 
-def _get_FanFiction(number_of_texts, fanfiction_ids = [], **kwargs):
-	fanfics = [FanFiction( text_id = idx ) for idx in fanfiction_ids]
-	fanfics.extend(FanFiction() for x in range(number_of_texts - len(fanfics)))
+def _scrape_fanfiction(number_of_texts = 0, fanfiction_ids = [], min_fanfiction_popularity = 0, **kwargs):
+	fanfics = [FanFiction( text_id = idx, min_popular = min_fanfiction_popularity ) for idx in fanfiction_ids]
+	more = max( number_of_texts - len(fanfics), 0 )
+	fanfics.extend(FanFiction() for x in range(more))
 	return fanfics
 
-def get_Texts(*args, **kwargs):
+def scrape_texts(*args, serialize = True, **kwargs):
 	"""
 
 	Examples:
 		get_Texts(3) - not recommended as searching for valid fanfiction can take a while
 		get_Texts(3, book_ids = [2701], fanfiction_ids = [37116025, 37601341, 37602172])
 	"""
-	return _get_Books(*args, **kwargs), _get_FanFiction(*args, **kwargs)
+	books = _scrape_books(*args, **kwargs)
+	fanfics = _scrape_fanfiction(*args, **kwargs)
+	if serialize:
+		with open('corpus.pkl', 'wb') as file:
+			pickle.dump(books, file)
+			pickle.dump(fanfics, file)
+	return books, fanfics
+
+def load_texts():
+	with open('corpus.pkl', 'rb') as file:
+		books = pickle.load(file)
+		fanfics = pickle.load(file)
+	return books, fanfics
