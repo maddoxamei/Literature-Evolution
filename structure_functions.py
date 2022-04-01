39e615f121f6e3af7393ab7d10d6449d27ed7e23
Thomas FitzGerald <thomas.fitzgerald23@ncf.edu>
2022-04-01 09:23:23 -0400

    Add files via upload

--- structure_functions.py
+++ structure_functions.py
@@ -1,12 +1,18 @@
-#This section is very incomplete.  convert_to_part_of_speech is about the only thing that works.
-
-
-from text_objects import Book, FanFiction
-from text_functions import scrape_texts
-import pickle
-import nltk
+import requests, random, sys, os
 import pandas as pd
+import pickle
+from bs4 import BeautifulSoup
+from unidecode import unidecode
 from collections import Counter
+
+import pyspark
+from pyspark.sql import SparkSession
+sc = pyspark.SparkContext("local[*]", "Sample Context")
+
+import nltk
+#nltk.download('punkt')
+#nltk.download('averaged_perceptron_tagger')
+
 
 def pos_only(x):
     #Gets the part of speech after pos tagging.  
@@ -28,72 +34,58 @@ def obj_to_string_list(book):
     #Only works with book currently.  Should also work with fanfiction.
     return(book[0].text)
     
+def isWord(tpl):
+    if(len(tpl[0])>1):
+        return(True)
+def isPunctuation(tpl):
+    if(len(tpl[0])==1):
+        return(True)       
+        
 def POS_prop(bookText):
-    #*** THIS FUNCTIONS NEEDS A LOT OF SPARK CODE***
     #Input: Text of a book or fanfiction, with each sentence as a list of strings
     #Output: List containing proportion of total words each part of speech makes up.
     
-    #Map to every element of Book.text/Fanfiction.text
-    rdd = bookText
-    #pos_list = rdd.map(convert_to_part_of_speech) #Spark
-    pos_list = list(map(convert_to_part_of_speech,rdd)) #Python
+    #Converts sentence list to summary count of POS (words+punctuation)
+    rdd = sc.parallelize(bookText)
+    rdd = rdd.map(convert_to_part_of_speech)
+    rdd = rdd.map(Counter)
+    rdd = rdd.map(lambda x:list(x.items())) #Convert from dictionary to list of tuples
+    rdd = rdd.flatMap(lambda x:x)
+    rdd = rdd.reduceByKey(lambda x, y: int(x) + int(y))
     
-    #Map these to all strings
-    pos_count = Counter(pos_list) #Returns dictionary of key/value pairs
-    pos_count_tuples = list(pos_count.items()) #converts above dictionary into a list of tuples
-    print(pos_count_tuples)
-    
-    #Reduce by key on pos_count_tuples
-    #Get sum of words
-    #Convert reduce by key output to proportions
-    #Sort proportion list
-    #Return list of proportions
-    print("Function not complete")    
-    
-    
-#Get book/fanfiction
-#test_run = scrape_texts(number_of_texts = 1)
-test_run = scrape_texts(book_ids = [2701], fanfiction_ids = [37116025])
+    #Words only
+    rddWords = rdd.filter(isWord)
+    rddWord_temp = rddWords.map(lambda x:x[1])
+    totalWords = rddWord_temp.reduce(lambda x,y: x+y)
 
+    #Punctuation Only
+    rddPunctuation = rdd.filter(isPunctuation)
+    rddPunc_temp = rddPunctuation.map(lambda x:x[1])
+    totalPunctuation = rddPunc_temp.reduce(lambda x,y: x+y)
 
+    #Proportions
+    rddWordProp = rddWords.map(lambda x:(x[0],x[1]/totalWords))
+    rddPunctuationProp = rddPunctuation.map(lambda x:(x[0],x[1]/totalPunctuation))
+    
+    lstOutput1 = rdd.collect()
+    lstOutput2 = rddWordProp.collect()
+    lstOutput3 = rddPunctuationProp.collect()
+    return((lstOutput1,lstOutput2,lstOutput3))
+    
 
 ###TEST CODE###
-#tplTest = ('Dog', 'NN')
-#strTest = "Have a heart that never hardens, and a temper that never tires, and a touch that never hurts"
-#strTest2 = "I loved her against reason, against promise, against peace, against hope, against happiness, against all discouragement that could be."
-#strTest3 = "To begin my life with the beginning of my life, I record that I was born (as I have been informed and believe) on a Friday, at twelve o'clock at night. It was remarked that the clock began to strike, and I began to cry, simultaneously."
-#lstTest = [strTest,strTest2,strTest3]
-
-book_text = obj_to_string_list(test_run[0])
-lstTest = book_text[750:753]
-print(lstTest)
-
-#Test for POS_prop
-pos_list = list(map(convert_to_part_of_speech,lstTest))
-print(pos_list)
-pos_count = [Counter(pos_list[0]),Counter(pos_list[1]),Counter(pos_list[2])]
-print(pos_count)
+strTest = "Have a heart that never hardens, and a temper that never tires, and a touch that never hurts"
+strTest2 = "I loved her against reason, against promise, against peace, against hope, against happiness, against all discouragement that could be."
+strTest3 = "To begin my life with the beginning of my life, I record that I was born (as I have been informed and believe) on a Friday, at twelve o'clock at night. It was remarked that the clock began to strike, and I began to cry, simultaneously."
+lstTest = [strTest,strTest2,strTest3]
+#print(lstTest)
 
 
-
-#Word+Part of speech as tuple
-#tokens = nltk.word_tokenize(strTest)
-#tagged = nltk.pos_tag(tokens)
-#print(tagged)
-
-#tagged = list(map(pos_only, tagged))
-#tagged = list(map(tuple_flipper, tagged))
-#print(tagged)
-
-#print(convert_to_part_of_speech(strTest))
-
-#pos_list = convert_to_part_of_speech(strTest)
-###END TEST CODE###
-
-
-
-
-
+#POS_prop test
+lstAnswer = POS_prop(lstTest)
+print(lstAnswer[0])
+print(lstAnswer[1])                                
+print(lstAnswer[2])
 
 def sentence_structure_complex_prop(bookText):
     #Input: Text of a book or fanfiction, with each sentence as a list of strings
