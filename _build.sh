1eca860471b986165035fbaa9d5fa3734c6df999
Mei Maddox <maddoxamei@gmail.com>
2022-04-14 07:28:09 -0400

    Workflow

--- _build.sh
+++ _build.sh
@@ -2,6 +2,6 @@
 
 set -ev
 
-Rscript -e "bookdown::render_book('R/index.Rmd', 'bookdown::gitbook')"
-#Rscript -e "bookdown::render_book('R/index.Rmd', 'bookdown::pdf_book')"
-#Rscript -e "bookdown::render_book('R/index.Rmd', 'bookdown::epub_book')"
+Rscript -e "bookdown::render_book('R/01-introduction.Rmd', 'bookdown::gitbook')"
+#Rscript -e "bookdown::render_book('R/01-introduction.Rmd', 'bookdown::pdf_book')"
+#Rscript -e "bookdown::render_book('R/01-introduction.Rmd', 'bookdown::epub_book')"
